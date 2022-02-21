b28d5f5bb331e1fdae0c88b05b7a18f84f4ff527
rNineleaps <rahul.kumar01@nineleaps.com>
2022-02-21 13:26:49 +0530

    Vendor dashboard

--- fitness-freak/src/App.js
+++ fitness-freak/src/App.js
@@ -1,12 +1,8 @@
 import Homepage from './Pages/Homepage';
-import SignupVendor from './Pages/Signup/SignupVendor'
-import Login from './Pages/Login/Login'
 function App() {
   return (
     <div className="App">
-      {/* <Homepage/> */}
-      {/* <SignupVendor /> */}
-      <Login />
+      <Homepage/>
     </div>
   );
 }
